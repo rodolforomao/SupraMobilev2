6bc953994a48c88a9cb0a6a6afa99a08eee89cc4
allessandrosj <allessandrosj@gmail.com>
2019-06-26 10:18:01 -0300

    novo projeto
    novo projeto

---
+++ src/app/lista-contratos/lista-contratos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-contratos',
+  templateUrl: './lista-contratos.page.html',
+  styleUrls: ['./lista-contratos.page.scss'],
+})
+export class ListaContratosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
